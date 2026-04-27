f108901fc282b08534e62783d5afeb596bd4d7a2
Folke Lemaitre <folke.lemaitre@gmail.com>
2026-04-27 08:55:05 +0200

    build: make devExports work properly with all runtimes. Remove attw, since that doesnt resolve publishConfig

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "tsdown"
 
+// - devExports:bun -> vitest uses dist/
+// - devExports:true -> attw complains since it doesnt resolve publishConfig
+// - devExports:source -> vitest only allows one devExport condition, so this would break bun
+// - exports.customExports -> works, but leaks sources in publishConfig and would also break bun
+// - devExports:true is the proper way to make it work. attw can be run on the CI using build artifacts
+
 export default defineConfig({
-  attw: { profile: "esm-only" },
+  // attw: { profile: "esm-only" }, // doesn't resolve publishConfig, so disable
   clean: true,
   deps: {
     alwaysBundle: ["slice-ansi", "string-width", "wrap-ansi"],
@@ -10,7 +16,7 @@ export default defineConfig({
     sourcemap: true,
   },
   exports: {
-    devExports: "bun",
+    devExports: true,
   },
   format: ["esm"],
   publint: true,
