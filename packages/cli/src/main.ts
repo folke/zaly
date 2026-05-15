518b4b1ae8633395c1ce0419d376783e24dc245e
Folke Lemaitre <folke.lemaitre@gmail.com>
2026-05-15 23:56:36 +0200

    feat(cli): set process title to zaly

--- packages/cli/src/main.ts
+++ packages/cli/src/main.ts
@@ -1,6 +1,8 @@
 import { runMain } from "citty"
 import { Cli, mainCommand } from "./cli.ts"
 
+process.title = "zaly"
+
 export async function main(argv: readonly string[] = process.argv.slice(2)): Promise<void> {
   const cli = new Cli()
   await runMain(mainCommand(cli), { rawArgs: [...argv] })
