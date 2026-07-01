ece712fd2c436a2546407858f60209cc5d15eacb
Folke Lemaitre <folke.lemaitre@gmail.com>
2026-07-01 18:20:41 +0200

    fix(shared/types): dont expand strings in Simplify

--- packages/shared/src/types.ts
+++ packages/shared/src/types.ts
@@ -1,11 +1,13 @@
 /** A type that can be either a value of type T or a Promise that resolves to a value of type T. */
 export type MaybePromise<T = void> = T | Promise<T>
 
+type SkipSimplify = Date | RegExp | Map<any, any> | Set<any> | string
+
 /** Simplify a type by flattening its properties. */
-export type Simplify<T> = { [K in keyof T]: T[K] } & {}
+export type Simplify<T> = T extends SkipSimplify ? T : { [K in keyof T]: T[K] } & {}
 
 /** Recursively simplify a type by flattening its properties. */
-export type SimplifyDeep<T> = T extends Date | RegExp | Map<any, any> | Set<any>
+export type SimplifyDeep<T> = T extends SkipSimplify
   ? T
   : T extends (infer U)[]
     ? SimplifyDeep<U>[]
